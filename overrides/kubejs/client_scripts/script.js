e40a406e9aa4fd73074732051bf5c8634877f760
DevDyna <78147916+DevDyna@users.noreply.github.com>
2023-03-28 14:56:34 +0200

    Add files via upload

---
+++ overrides/kubejs/client_scripts/script.js
@@ -0,0 +1,13 @@
+// priority: 0
+
+console.info('Hello, World! (You will see this line every time client resources reload)')
+
+JEIEvents.hideItems(event => {
+	// Hide items in JEI here
+	// event.hide('minecraft:cobblestone')
+})
+
+ItemEvents.tooltip(event => {
+  // Add tooltip to all of these items
+  event.add('randomium:randomium_ore', 'Mine me only with silent gear pickaxe tier iron or over!')
+  })
